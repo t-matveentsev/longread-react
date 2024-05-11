fdad90959702a21d173b6cb3b31d263140b788d3
t-matveentsev <tmatveentsev@gmail.com>
2024-05-11 15:26:54 +0200

    PropTypes, style

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,13 @@
+import PropTypes from "prop-types";
+
+const alertStyles = {
+  margin: 8,
+  padding: "12px 16px",
+  borderRadius: 4,
+  backgroundColor: "gray",
+  color: "white",
+};
+
 const Product = ({
   name,
   imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
@@ -5,11 +15,17 @@ const Product = ({
 }) => {
   return (
     <div>
-      <h2>{name}</h2>
+      <h2 style={alertStyles}>{name}</h2>
       <img src={imgUrl} alt={name} width="640" />
       <p>Price: {price} credits</p>
     </div>
   );
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
 };
 
 export default Product;
